c89f693ab840879dbc02ce4a46434eaf6c9e2153
rushi-0369 <23211a0556@bvrit.ac.in>
2026-03-18 16:00:45 +0530

    MIK

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -137,4 +137,28 @@ int trap(vector<int>& height) {
 }
 
 //16. 3 sum closest
-
+int threeSumClosest(vector<int> &nums, int target){
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    int closestSum = nums[0] + nums[1] + nums[2];
+    for(int k = 0; k <= n-3; k++){
+        int i = k + 1;
+        int j = n - 1;
+        while(i < j){
+            int sum = nums[i] + nums[j] + nums[k];
+            if(abs(target - sum) < abs(target-closestSum)){
+                closestSum = sum;
+            }
+            if(sum < target){
+                i++;
+            }
+            else if(sum > target){
+                j--;
+            }
+            else{
+                return sum;
+            }
+        }
+    }
+    return closestSum;
+}
