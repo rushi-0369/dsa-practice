4b0e1811d120fedd4306cd6324b3e8241699988c
rushi-0369 <23211a0556@bvrit.ac.in>
2026-05-06 00:41:33 +0530

    W

--- Binary Search/binarySearch.cpp
+++ Binary Search/binarySearch.cpp
@@ -351,6 +351,20 @@ int findKRotation(vector<int>& a){
     }
     return index;
 }
+int findKRotation(vector<int>& a){
+    int st = 0;
+    int end = a.size() - 1;
+    while(st < end){
+        int mid = st + (end-st)/2;
+        if(a[mid] < a[end]){
+            end = mid;
+        }
+        else{
+            st = mid + 1;
+        }
+    }
+    return end;
+}
 
 //peak
 int peak(vector<int>& a){
@@ -443,6 +457,37 @@ int singleElement(vector<int> &a){
         }
     }
     return -1;
+}
+int singleElement(vector<int>&a){
+    int st = 0;
+    int end = a.size()-1;
+    while(st < end){
+        int mid = st + (end - st)/2;
+        bool isEven;
+        if((end-mid)%2 == 0){
+            isEven = true;
+        }
+        else{
+            isEven = false;
+        }
+        if(a[mid]==a[mid+1]){
+            if(isEven){
+                st = mid + 2;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        else{
+            if(isEven){
+                end = mid;
+            }
+            else{
+                st = mid + 1;
+            }
+        }
+    }
+    return a[end];
 }
 
 //BS on answers
